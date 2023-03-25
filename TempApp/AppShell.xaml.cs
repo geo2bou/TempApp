6513c75d0ea288597daefa8cbb6ac1d124db49a5
George Bourazanis <geo2bu@gmail.com>
2023-03-25 23:45:40 +0200

    Add project files.

---
+++ TempApp/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TempApp.Views;
+using Xamarin.Forms;
+
+namespace TempApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
