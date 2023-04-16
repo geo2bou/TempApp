7c8d71694d0679b3add79a4d84767aaf150cde70
George Bourazanis <geo2bu@gmail.com>
2023-04-16 23:35:55 +0300

    Add project files.

---
+++ TempApp/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TempApp.Themes
+{	
+	public partial class DarkTheme : ContentPage
+	{	
+		public DarkTheme ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ TempApp/Themes/LightTheme.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TempApp.Themes
+{	
+	public partial class LightTheme : ContentPage
+	{	
+		public LightTheme ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
